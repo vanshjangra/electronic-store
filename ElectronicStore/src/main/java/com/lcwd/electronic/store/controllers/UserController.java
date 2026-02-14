7050132559bd90e091379d1807a446c232bd1c1a
vanshjangra <vanshjangra586@gmail.com>
2026-02-14 14:54:59 +0530

    finishing user controller

---
+++ ElectronicStore/src/main/java/com/lcwd/electronic/store/controllers/UserController.java
@@ -0,0 +1,56 @@
+package com.lcwd.electronic.store.controllers;
+
+import com.lcwd.electronic.store.dtos.UserDto;
+import com.lcwd.electronic.store.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
+        UserDto userDto1 = userService.createUser(userDto);
+        return new ResponseEntity<>(userDto1, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserDto> updateUser(@PathVariable("userId") String userId,
+                                              @RequestBody UserDto userDto){
+        UserDto updatedUserDto = userService.updateUser(userDto, userId);
+        return new ResponseEntity<>(updatedUserDto, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<String> deleteUser(@PathVariable String userId){
+        userService.deleteUser(userId);
+        return new ResponseEntity<>("User is deleted successfully!", HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDto>> getAllUsers(){
+        return new ResponseEntity<>(userService.getAllUser(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDto> getUser(@PathVariable String userId){
+        return new ResponseEntity<>(userService.getUserById(userId), HttpStatus.OK);
+    }
+
+    @GetMapping("/email/{email}")
+    public ResponseEntity<UserDto> getUserByEmail(@PathVariable String email){
+        return new ResponseEntity<>(userService.getUserByEmail(email), HttpStatus.OK);
+    }
+
+    @GetMapping("/search/{keywords}")
+    public ResponseEntity<List<UserDto>> searchUser(@PathVariable String keywords){
+        return new ResponseEntity<>(userService.searchUser(keywords), HttpStatus.OK);
+    }
+}
