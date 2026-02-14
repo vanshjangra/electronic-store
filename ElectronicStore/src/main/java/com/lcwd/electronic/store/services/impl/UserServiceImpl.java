d26824d075861e40d4920928cbbad4bc10fd5cdb
vanshjangra <vanshjangra586@gmail.com>
2026-02-14 09:50:06 +0530

    creating mapping dto mapping manually

---
+++ ElectronicStore/src/main/java/com/lcwd/electronic/store/services/impl/UserServiceImpl.java
@@ -0,0 +1,85 @@
+package com.lcwd.electronic.store.services.impl;
+
+import com.lcwd.electronic.store.dtos.UserDto;
+import com.lcwd.electronic.store.entities.User;
+import com.lcwd.electronic.store.repositories.UserRepository;
+import com.lcwd.electronic.store.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        String userId = UUID.randomUUID().toString();
+        userDto.setUserId(userId);
+
+        User user = dtoToEntity(userDto);
+        User savedUser = userRepository.save(user);
+
+        UserDto newDto = entityToDto(savedUser);
+        return newDto;
+    }
+
+    @Override
+    public UserDto updateUser(UserDto userDto, String userId) {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(String userId) {
+
+    }
+
+    @Override
+    public List<UserDto> getAllUser() {
+        return List.of();
+    }
+
+    @Override
+    public UserDto getUserById(String userId) {
+        return null;
+    }
+
+    @Override
+    public UserDto getUserByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public List<UserDto> searchUser(String keyword) {
+        return List.of();
+    }
+
+    private UserDto entityToDto(User savedUser){
+        UserDto userDto = UserDto.builder()
+                .userId(savedUser.getUserId())
+                .name(savedUser.getName())
+                .email(savedUser.getEmail())
+                .password(savedUser.getPassword())
+                .about(savedUser.getAbout())
+                .gender(savedUser.getGender())
+                .imageName(savedUser.getImageName())
+                .build();
+        return userDto;
+    }
+
+    private User dtoToEntity(UserDto userDto){
+        User user = User.builder()
+                .userId(userDto.getUserId())
+                .name(userDto.getName())
+                .email(userDto.getEmail())
+                .password(userDto.getPassword())
+                .about(userDto.getAbout())
+                .gender(userDto.getGender())
+                .imageName(userDto.getImageName())
+                .build();
+        return user;
+    }
+}
