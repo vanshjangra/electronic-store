7410f7583045530695952d9cc641e2e132d3edf5
vanshjangra <vanshjangra586@gmail.com>
2026-02-14 08:46:35 +0530

    create user entity explanation

---
+++ ElectronicStore/src/main/java/com/lcwd/electronic/store/entities/User.java
@@ -0,0 +1,29 @@
+package com.lcwd.electronic.store.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String userId;
+    @Column(name = "user_name")
+    private String name;
+    @Column(name = "user_email", unique = true)
+    private String email;
+    @Column(name = "user_password", length = 10)
+    private String password;
+    private String gender;
+    @Column(length = 1000)
+    private String about;
+    @Column(name = "user_image_name")
+    private String imageName;
+}
