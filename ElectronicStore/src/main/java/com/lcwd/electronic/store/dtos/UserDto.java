6cc13696ca2ac3c36de26ab9d7b910f8b56114ab
vanshjangra <vanshjangra586@gmail.com>
2026-02-15 16:30:57 +0530

    @pattern for regular expressions for validation

--- ElectronicStore/src/main/java/com/lcwd/electronic/store/dtos/UserDto.java
+++ ElectronicStore/src/main/java/com/lcwd/electronic/store/dtos/UserDto.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Getter
@@ -15,7 +16,8 @@ public class UserDto {
     private String userId;
     @Size(min = 3, max = 20, message = "Invalid Name!")
     private String name;
-    @Email(message = "Invalid User Email!")
+//    @Email(message = "Invalid User Email!")
+    @Pattern(regexp = "^[a-z0-9][-a-z0-9._]+@([-a-z0-9]+\\.)+[a-z]{2,5}$", message = "Invalid User Email!")
     @NotBlank(message = "Email is required!")
     private String email;
     @NotBlank(message = "Password is required!")
