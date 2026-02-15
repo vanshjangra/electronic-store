e1822505d6494e26cd4b107db4ad590324f460f1
vanshjangra <vanshjangra586@gmail.com>
2026-02-15 19:49:52 +0530

    implementation api pagination

--- ElectronicStore/src/main/java/com/lcwd/electronic/store/controllers/UserController.java
+++ ElectronicStore/src/main/java/com/lcwd/electronic/store/controllers/UserController.java
@@ -44,8 +44,9 @@ public class UserController {
     }
 
     @GetMapping
-    public ResponseEntity<List<UserDto>> getAllUsers(){
-        return new ResponseEntity<>(userService.getAllUser(), HttpStatus.OK);
+    public ResponseEntity<List<UserDto>> getAllUsers(@RequestParam(value = "pageNumber", defaultValue = "0", required = false) int pageNumber,
+                                                     @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize){
+        return new ResponseEntity<>(userService.getAllUser(pageNumber, pageSize), HttpStatus.OK);
     }
 
     @GetMapping("/{userId}")
