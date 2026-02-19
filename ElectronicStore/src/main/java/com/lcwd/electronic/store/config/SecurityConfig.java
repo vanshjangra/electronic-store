016f313ac16dd0fcab51f275ecaa51fd0ade1a0b
vanshjangra <vanshjangra586@gmail.com>
2026-02-19 17:26:21 +0530

    form based login customicatztion

--- ElectronicStore/src/main/java/com/lcwd/electronic/store/config/SecurityConfig.java
+++ ElectronicStore/src/main/java/com/lcwd/electronic/store/config/SecurityConfig.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -32,6 +34,23 @@ public class SecurityConfig {
 //
 //        return new InMemoryUserDetailsManager(normal, admin);
 //    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .formLogin()
+//                .loginPage("login.html")
+//                .loginProcessingUrl("/process-url")
+//                .defaultSuccessUrl("/dashboard")
+//                .failureUrl("error")
+//                .and()
+//                .logout()
+//                .logoutUrl("/logout");
+        return http.build();
+    }
 
     @Bean
     public DaoAuthenticationProvider authenticationProvider(){
