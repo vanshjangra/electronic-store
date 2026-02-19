c14ddc66d5a18ae63494b380d49e05d89d12b4d8
vanshjangra <vanshjangra586@gmail.com>
2026-02-19 16:36:45 +0530

    authenticating from database using spring security

--- ElectronicStore/src/main/java/com/lcwd/electronic/store/config/SecurityConfig.java
+++ ElectronicStore/src/main/java/com/lcwd/electronic/store/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.lcwd.electronic.store.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,21 +13,32 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfig {
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+//    @Bean
+//    public UserDetailsService userDetailsService(){
+//        UserDetails normal = User.builder()
+//                .username("vanshjangra")
+//                .password(passwordEncoder().encode("vanshjangra"))
+//                .roles("NORMAL")
+//                .build();
+//
+//        UserDetails admin = User.builder()
+//                .username("sachinjangra")
+//                .password(passwordEncoder().encode("sachinjangra"))
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(normal, admin);
+//    }
+
     @Bean
-    public UserDetailsService userDetailsService(){
-        UserDetails normal = User.builder()
-                .username("vanshjangra")
-                .password(passwordEncoder().encode("vanshjangra"))
-                .roles("NORMAL")
-                .build();
-
-        UserDetails admin = User.builder()
-                .username("sachinjangra")
-                .password(passwordEncoder().encode("sachinjangra"))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(normal, admin);
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(this.userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
     }
 
     @Bean
--- ElectronicStore/src/main/java/com/lcwd/electronic/store/controllers/AuthController.java
+++ ElectronicStore/src/main/java/com/lcwd/electronic/store/controllers/AuthController.java
@@ -1,9 +1,10 @@
 package com.lcwd.electronic.store.controllers;
 
+import com.lcwd.electronic.store.dtos.UserDto;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +18,12 @@ public class AuthController {
     @Autowired
     private UserDetailsService userDetailsService;
 
+    @Autowired
+    private ModelMapper mapper;
+
     @GetMapping("/current")
-    public ResponseEntity<UserDetails> getCurrentUser(Principal principal){
+    public ResponseEntity<UserDto> getCurrentUser(Principal principal){
         String name = principal.getName();
-        return new ResponseEntity<>(userDetailsService.loadUserByUsername(name), HttpStatus.OK);
+        return new ResponseEntity<>(mapper.map(userDetailsService.loadUserByUsername(name), UserDto.class), HttpStatus.OK);
     }
 }
