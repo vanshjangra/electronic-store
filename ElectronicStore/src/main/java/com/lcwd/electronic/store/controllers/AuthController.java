72b959722810965b1272de8606879b2e85dd80e3
vanshjangra <vanshjangra586@gmail.com>
2026-02-20 09:42:09 +0530

    completing jwt implementation and testing

--- ElectronicStore/src/main/java/com/lcwd/electronic/store/controllers/AuthController.java
+++ ElectronicStore/src/main/java/com/lcwd/electronic/store/controllers/AuthController.java
@@ -1,14 +1,21 @@
 package com.lcwd.electronic.store.controllers;
 
+import com.lcwd.electronic.store.dtos.JwtRequest;
+import com.lcwd.electronic.store.dtos.JwtResponse;
 import com.lcwd.electronic.store.dtos.UserDto;
+import com.lcwd.electronic.store.exceptions.BadApiRequestException;
+import com.lcwd.electronic.store.security.JwtHelper;
+import com.lcwd.electronic.store.services.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
@@ -19,11 +26,46 @@ public class AuthController {
     private UserDetailsService userDetailsService;
 
     @Autowired
-    private ModelMapper mapper;
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private AuthenticationManager manager;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private JwtHelper helper;
 
     @GetMapping("/current")
     public ResponseEntity<UserDto> getCurrentUser(Principal principal){
         String name = principal.getName();
-        return new ResponseEntity<>(mapper.map(userDetailsService.loadUserByUsername(name), UserDto.class), HttpStatus.OK);
+        return new ResponseEntity<>(modelMapper.map(userDetailsService.loadUserByUsername(name), UserDto.class), HttpStatus.OK);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponse> login(@RequestBody JwtRequest request){
+        this.doAuthenticate(request.getEmail(), request.getPassword());
+        UserDetails userDetails = userDetailsService.loadUserByUsername(request.getEmail());
+        String token = this.helper.generateToken(userDetails);
+
+        UserDto userDto = modelMapper.map(userDetails, UserDto.class);
+
+        JwtResponse response = JwtResponse.builder()
+                .jwtToken(token)
+                .user(userDto)
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    private void doAuthenticate(String email, String password){
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(email, password);
+
+        try {
+            manager.authenticate(authentication);
+        }
+        catch (BadCredentialsException e){
+            throw new BadApiRequestException("Invalid Username or Password!");
+        }
     }
 }
---
+++ ElectronicStore/src/main/java/com/lcwd/electronic/store/dtos/JwtResponse.java
@@ -0,0 +1,14 @@
+package com.lcwd.electronic.store.dtos;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class JwtResponse {
+    private String jwtToken;
+    private UserDto user;
+}
