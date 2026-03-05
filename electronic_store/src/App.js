78d5024062b8012c7fd28f6bdb8ed51570c29301
vanshjangra <vanshjangra586@gmail.com>
2026-03-05 09:34:04 +0530

    understanding how we create websites

--- electronic_store/src/App.js
+++ electronic_store/src/App.js
@@ -3,17 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Subscribe to LearnCodeWithDurgesh youtube channel.
-        </a>
-        Wow this is amazing
-      </header>
+    <div>
+      <button>Hey Click me</button>
     </div>
   );
 }
