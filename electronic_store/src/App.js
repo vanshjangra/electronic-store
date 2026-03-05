86f13d61d96aa301f5f6e1f6a307ecd54c88ce11
vanshjangra <vanshjangra586@gmail.com>
2026-03-05 10:02:29 +0530

    about react bootstrap | installing and using react bootstrap

--- electronic_store/src/App.js
+++ electronic_store/src/App.js
@@ -1,10 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Modal } from 'react-bootstrap';
+import { useState } from 'react';
 
 function App() {
+const [show, setShow] = useState(false);
+const handleClose = () => setShow(false);
+const handleShow = () => setShow(true);
+
   return (
     <div>
-      <button>Hey Click me</button>
+      <Button variant='success' onClick={handleShow} size='sm'>
+        Hey Click Me
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant='secondary' onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant='primary' onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
