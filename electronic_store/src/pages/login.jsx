14b81b0e2484a4fbcf430b5b639c52ef47f973c9
vanshjangra <vanshjangra586@gmail.com>
2026-03-06 17:09:50 +0530

    login form | create login form step by step

--- electronic_store/src/pages/login.jsx
+++ electronic_store/src/pages/login.jsx
@@ -1,10 +1,58 @@
+import { Button, Card, Col, Container, Form, Row } from "react-bootstrap"
 import Base from "../components/Base"
+import logo from "../assets/logo.png"
+import { NavLink } from "react-router-dom"
 
 const Login = () => {
+  const loginForm = () => {
+    return (
+      <Container>
+        <Row>
+          <Col md={{span: 8, offset: 2}}>
+          <Card className="my-3 border-0 shadow"style={{
+            position: "relative",
+            top: -60
+          }}>
+            <Card.Body>
+              <Container className="text-center mb-3">
+                <img src={logo} alt="Store logo" width={80} height={80}/>
+              </Container>
+
+              <h3 className="text-center text-uppercase">Store Login</h3>
+
+              <Form>
+                <Form.Group className="mb-3">
+                  <Form.Label>Enter Email</Form.Label>
+                  <Form.Control type="text" placeholder="Enter here"/>
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                  <Form.Label>Enter Password</Form.Label>
+                  <Form.Control type="password" placeholder="Enter here"/>
+                </Form.Group>
+              </Form>
+
+              <Container className="text-center">
+                {/* <p>Forget Password! <a href="/forget">Click here</a></p> */}
+                <p>If not registered! <NavLink to="/register">Click here</NavLink></p>
+              </Container>
+
+              <Container className="text-center">
+                <Button className="" variant="success">Login</Button>
+                <Button className="ms-2" variant="danger">Reset</Button>
+              </Container>
+            </Card.Body>
+          </Card>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+
   return (
-    <Base title="Electro Store / Login"
-          description="Login Here">
-         <h1>This is login page</h1>
+    <Base title="Electro Store / Login Page"
+          description={null}>
+         {loginForm()}
     </Base>
   )
 }
