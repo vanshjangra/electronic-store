5925f7c759646636dd0fcef37c095c13a1d95811
vanshjangra <vanshjangra586@gmail.com>
2026-03-07 16:25:17 +0530

    managing admin specific routes

--- electronic_store/src/App.js
+++ electronic_store/src/App.js
@@ -15,6 +15,9 @@ import Register from './pages/register';
 import Home from './pages/users/home';
 import UserProvider from './context/user.provider';
 import Order from './pages/users/order';
+import AdminDashboard from "./pages/admin/admin.dashboard"
+import AdminHome from "./pages/admin/admin.home"
+import AddProduct from "./pages/admin/add.product"
 
 function App() {
   return (
@@ -31,11 +34,17 @@ function App() {
       <Route path='/cart' element={<Cart/>}/>
       <Route path='/login' element={<Login/>}/>
       <Route path='/register' element={<Register/>}/>
+
       <Route path='/users' element={<Dashboard/>}>
         <Route path='home' element={<Home/>}/>
         <Route path='profile' element={<Profile/>}/>
         <Route path='about' element={<AboutUser/>}/>
         <Route path='orders' element={<Order/>}/>
+      </Route>
+
+      <Route path='/admin' element={<AdminDashboard/>}>
+        <Route path='home' element={<AdminHome/>}/>
+        <Route path='add-product' element={<AddProduct/>}/>
       </Route>
     </Routes>
     </BrowserRouter>
---
+++ electronic_store/src/pages/admin/add.product.jsx
@@ -0,0 +1,9 @@
+const AddProduct = () => {
+  return (
+    <div>
+      This is add product
+    </div>
+  )
+}
+
+export default AddProduct
