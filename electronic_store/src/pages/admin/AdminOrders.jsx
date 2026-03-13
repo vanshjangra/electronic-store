c917f348217a590eef8b972e47d0ba591ff439f4
vanshjangra <vanshjangra586@gmail.com>
2026-03-13 10:46:46 +0530

    loading orders using async and await call to server

--- electronic_store/src/pages/admin/AdminOrders.jsx
+++ electronic_store/src/pages/admin/AdminOrders.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react"
+import { getAllOrders } from "../../services/OrderService"
+import { ADMIN_ORDER_PAGE_SIZE } from "../../services/helper.service"
+
 const AdminOrders = () => {
+  const [ordersData, setOrdersData] = useState(undefined)
+
+  useEffect(() => {
+    getOrdersLocally();
+  }, [])
+
+  const getOrdersLocally = async () => {
+    try {
+      const data = await getAllOrders(0, ADMIN_ORDER_PAGE_SIZE, 'orderedDate', 'desc');
+      console.log(data)
+      setOrdersData(data) 
+    }
+    catch (e) {
+      console.log("Error")
+      console.log(e)
+    }
+  }
+
   return (
     <>
     <p>Admin orders</p>
+    {JSON.stringify(ordersData)}
     </>
   )
 }
--- electronic_store/src/services/OrderService.jsx
+++ electronic_store/src/services/OrderService.jsx
@@ -1,6 +1,6 @@
 import {privateAxios} from './axios.service';
 
-export const getAllOrders = async () => {
-    let result = await privateAxios.get(`/orders`);
+export const getAllOrders = async (pageNumber, pageSize, sortBy, sortDir) => {
+    let result = await privateAxios.get(`/orders?pageNumber=${pageNumber}&pageSize=${pageSize}&sortBy=${sortBy}&sortDir=${sortDir}`);
     return result.data;
 };
