14faf684bbad11624076a9e80791c02a35f64249
vanshjangra <vanshjangra586@gmail.com>
2026-03-14 17:42:23 +0530

    working on store category | showing dymaic categories store

--- electronic_store/src/components/users/Store.jsx
+++ electronic_store/src/components/users/Store.jsx
@@ -1,19 +1,73 @@
-import React from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
-
-const categoryView = () => {
-    return (
-        <h1>This is category view</h1>
-    )
-}
-
-const productsView = () => {
-    return (
-        <h1>This is product view</h1>
-    )
-}
+import { Col, Container, ListGroup, Row } from 'react-bootstrap'
+import { getCategories } from '../../services/CategoryService'
+import { useEffect, useState } from 'react'
+import defaultCategoryImage from '../../assets/default_profile.jpg'
 
 function Store(){
+    const [categories, setCategories] = useState(null)
+    const [products, setProducts] = useState(null)
+
+    useEffect(() => {
+        loadCategories(0, 100000)
+    }, [])
+
+    const loadCategories = (pageNumber, pageSize) => {
+        getCategories(pageNumber, pageSize)
+        .then(data => {
+            console.log(data)
+            setCategories({...data})
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
+
+    const loadProducts = () => {
+
+    }
+
+    const categoryView = () => {
+      return categories && (
+        <>
+        <ListGroup variant='flush' className='stick-top'>
+            <ListGroup.Item action>
+                <img src={defaultCategoryImage} alt={'default category image'} className=' rounded-circle'
+                     style={{
+                        width: "40px",
+                        height: "40px",
+                        objectFit: "cover"
+                     }}
+                     onError={event => {
+                        event.currentTarget.setAttribute('src', defaultCategoryImage)
+                     }}/>
+                <span className='ms-2'>All Products</span>
+            </ListGroup.Item>
+
+            {categories.content.map(cat => (
+            <ListGroup.Item action key={cat.categoryId}>
+                <img src={cat.coverImage} alt={cat.title} className=' rounded-circle'
+                     style={{
+                        width: "40px",
+                        height: "40px",
+                        objectFit: "cover"
+                     }}
+                     onError={event => {
+                        event.currentTarget.setAttribute('src', defaultCategoryImage)
+                     }}/>
+                <span className='ms-2'>{cat.title}</span>
+            </ListGroup.Item>
+            ))}
+        </ListGroup>
+        </>
+      )
+    }
+
+    const productsView = () => {
+      return (
+        <h1>This is product view</h1>
+      )
+    }
+
   return (
     <Container fluid className='px-5 pt-5'>
         <Row>
